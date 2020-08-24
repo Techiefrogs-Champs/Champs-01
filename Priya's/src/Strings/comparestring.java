e1eff55184347764a58ae98870724db118b08860
Your Name <krishnapriyanekkanti@gmail.com>
2020-08-24 20:31:58 +0530

    my second commit

---
+++ Priya's/src/Strings/comparestring.java
@@ -0,0 +1,15 @@
+package Strings;
+
+public class comparestring {
+
+    public static void main(String[] args) {
+
+        String style = "priya";
+        String style2 = "priya";
+
+        if(style == style2)
+            System.out.println("Equal");
+        else
+            System.out.println("Not Equal");
+    }
+}
