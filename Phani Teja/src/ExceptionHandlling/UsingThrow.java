f449fab822fb44cb17c52958ed69d03c1567068b
Soumya028 <soumyachoudary028@gmail.com>
2020-08-26 12:56:56 +0530

    second

--- Phani Teja/src/ExceptionHandlling/UsingThrow.java
+++ Phani Teja/src/ExceptionHandlling/UsingThrow.java
@@ -11,7 +11,7 @@ public class UsingThrow {
         }
             
             public static void main(String[] args) {
-                validate(34);
+                validate(32);
             System.out.println("run the code");
         }
     
