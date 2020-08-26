4900f982cfe7a7fe4b92176de6ee04e24ab73498
SonyMullapudi <sonymullapudi10@gmail.com>
2020-08-26 15:29:18 +0530

    second commit

---
+++ sri devi/src/Array/End13.java
@@ -0,0 +1,36 @@
+public class End13 {
+    
+   public static void main(String[] args) {
+       
+     int a[][]={{1,4,5},{2,5,7},{3,6,8}};
+     int b[][]={{2,3,4},{5,6,7},{8,9,0}};
+
+    int c[][]=new int[3][3];
+
+      for(int i=0;i<3;i++){
+
+      for(int j=0;j<3;j++){
+
+        c[i][j]=a[i][j]-b[i][j];
+        System.out.print(c[i][j]+" ");
+      }
+        System.out.println();
+      }
+
+
+
+
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ sri devi/src/Strings/Start1.java
@@ -0,0 +1,18 @@
+public class Start1 {
+  public static void main(String[] args) {
+    
+    StringBuffer o=new StringBuffer(); 
+     o.append("this is");
+     o.append("new");
+     o.append("object");
+     System.out.println("string buffer="+o);
+    o=new StringBuffer();
+      System.out.println("new object created for string buffer="+o);
+
+  } 
+    
+
+
+
+
+}
