87b5df8cdadbb2422f59122731afc0521787b0b9
Krushival <krushivalreddykancharla@gmail.com>
2020-09-10 22:58:09 +0530

    krushi second commit

--- krushival/src/Helloworld.java
+++ krushival/src/Helloworld.java
@@ -1,5 +1,14 @@
-class Helloworld{
-    public static void main(String[] args){
-        System.out.println("Hello world");
-    }
-}
+
+class Output 
+	    {
+	        public static void main(String args[]) 
+	        {    
+	             int x , y = 1;
+	             x = 10;
+	             if (x != 10 && x / 0 == 0)
+	                 System.out.println(y);
+	             else
+	                 System.out.println(++y);
+	        } 
+	    }
+﻿​
