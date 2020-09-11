dbc6daff2b0fab7fcfff20b259431dfc2b8ab9ad
Krushival <krushivalreddykancharla@gmail.com>
2020-09-11 12:41:17 +0530

    krushi 4th commit

---
+++ krushival/Inheritance/Base.java
@@ -0,0 +1,20 @@
+ class Base {
+    void disp(){
+        System.out.println("this is base class");
+      }
+    }
+    
+    class Derived{
+      void disp(){
+        System.out.println("this is sub class");
+        super.disp();
+      }
+    }
+    
+     class Main {
+      public static void main(String[] args) {
+        Derived d = new Derived();
+        d.disp();
+      }
+    }
+}
