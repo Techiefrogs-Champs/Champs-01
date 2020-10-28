8cf76a70f17f45307f0ce133c1b1e6bddc69c4d5
LakshmiTejaswi18 <lakshmitejaswi18@gmail.com>
2020-10-28 20:14:01 +0530

    sql

---
+++ Tejaswi/Basics/News.java
@@ -0,0 +1,16 @@
+package Basics;
+
+class News
+{
+     public static void main(String[] args) 
+     {
+         final char ch= 'A';
+         switch(65)
+         {
+             case ch:
+             System.out.println("65");
+         case 66:
+         System.out.println("66");
+         }
+     }
+}
